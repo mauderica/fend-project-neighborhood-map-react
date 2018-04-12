bbd0f9d1a5fe1abafa975c93a7f393ad7069df3e
Mauderica <maude.chalin@gmail.com>
2018-04-12 14:47:46 -0400

    docs: add title & location filter

--- my-neighborhood-map-app/src/App.js
+++ my-neighborhood-map-app/src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    query: '',
+  }
+
+  updateQuery = (userInput) => {
+    this.setState({ query: userInput });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Featured Neighborhood: Nashville</h1>
         </header>
+        <div className="location-filter">
+          <input type="text" placeholder="Filter locations by..."
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)} />
+        </div>
       </div>
     );
   }
