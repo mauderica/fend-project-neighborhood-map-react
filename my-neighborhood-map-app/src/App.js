65eab94d11112fde336d8925d1b92e090c51fecb
mauderica <maude.chalin@gmail.com>
2018-04-14 18:01:02 -0400

    Incorporate google maps (#2)
    * chore: install google-maps-react
    
    * feat: load Google Maps
    
    * feat: add locations & markers to map
    
    * docs: add generic infoWindow to marker onClick

--- my-neighborhood-map-app/src/App.js
+++ my-neighborhood-map-app/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { GoogleApiWrapper } from 'google-maps-react';
 import ListView from './ListView';
 import MapContainer from './MapContainer';
 
 class App extends Component {
   state = {
     query: '',
+    locations: [
+      { name: "The Wild Cow", location: { lat: 36.1824707, lng: -86.7354019 } },
+      { name: "Sunflower Cafe", location: { lat: 36.1132908, lng: -86.7678391 } },
+      { name: "Graze Nashville", location: { lat: 36.1824427, lng: -86.73558109999999 } },
+      { name: "AVO", location: { lat: 36.1521497, lng: -86.8201611 } },
+      { name: "The Wild Muffin", location: { lat: 36.129007, lng: -86.90273000000002 } },
+      { name: "The Southern V Bakery and To-Go Eatery", location: { lat: 36.1804356, lng: -86.80700669999999 } },
+    ],
   }
 
   updateQuery = (userInput) => {
@@ -26,10 +35,12 @@ class App extends Component {
           </div>
           <ListView />
         </div>
-        <MapContainer />
+        <MapContainer google={this.props.google} locations={this.state.locations} />
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCGTckzBEuuWMFPGef0OxueA3RyPGYO4ws',
+})(App);
--- my-neighborhood-map-app/src/MapContainer.js
+++ my-neighborhood-map-app/src/MapContainer.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 
-class MapContainer extends Component {
+export default class MapContainer extends Component {
 
-  render() {
-    return (
-      <div className="map-wrapper">
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.loadMap();
+    }
+
+    componentDidUpdate() {
+        this.loadMap();
+    }
+
+    // Note: The following resources were consulted to develop the code in the function below:
+    // https://medium.com/front-end-hacking/simplified-google-maps-api-in-a-react-app-46981441d2c9
+    // https://developers.google.com/maps/documentation/javascript/tutorial
+    // https://developers.google.com/maps/documentation/javascript/infowindows
+    loadMap() {
+        if (this.props && this.props.google) {
+            const { google } = this.props;
+            const mapRef = this.refs.map;
+            const node = ReactDOM.findDOMNode(mapRef);
+
+            const mapConfig = {
+                center: { lat: 36.1521497, lng: -86.8201611 },
+                zoom: 12,
+                mapTypeId: 'roadmap',
+            }
+
+            const map = new google.maps.Map(node, mapConfig);
+
+            // Create InfoWindow:
+            let infoWindow = new google.maps.InfoWindow({
+                content: "Sample Info About Restaurant :)"
+            });
+
+            // Add Markers to Map:
+            this.props.locations.forEach(location => {
+                const marker = new google.maps.Marker({
+                    position: location.location,
+                    map: map,
+                    title: location.name
+                });
+                marker.addListener('click', function () {
+                    infoWindow.open(map, marker);
+                });
+            })
+
+        }
+    }
+
+    render() {
+        // const style = {
+        //     width: '90vw',
+        //     height: '75vh'
+        //   }
+
+        return (
+            <div ref="map" /*style={style}*/ className="map-wrapper">
+                loading map...
+            </div>
+        );
+    }
 }
-
-export default MapContainer;
