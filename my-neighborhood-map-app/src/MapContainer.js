179112f69411d3a1d2af0bb669a3b771a5b5691b
Mauderica <maude.chalin@gmail.com>
2018-04-14 17:00:22 -0400

    feat: add locations & markers to map

--- my-neighborhood-map-app/src/MapContainer.js
+++ my-neighborhood-map-app/src/MapContainer.js
@@ -6,27 +6,36 @@ export default class MapContainer extends Component {
     componentDidMount() {
         this.loadMap();
     }
-    
+
     componentDidUpdate() {
         this.loadMap();
     }
 
+    // Note: The following resources were consulted to develop the code in the function below:
+    // https://medium.com/front-end-hacking/simplified-google-maps-api-in-a-react-app-46981441d2c9
+    // https://developers.google.com/maps/documentation/javascript/tutorial
     loadMap() {
         if (this.props && this.props.google) {
             const { google } = this.props;
-
-            // Note: The below was written in part with the help of the following blog article
-            // https://medium.com/front-end-hacking/simplified-google-maps-api-in-a-react-app-46981441d2c9
             const mapRef = this.refs.map;
             const node = ReactDOM.findDOMNode(mapRef);
 
             const mapConfig = {
-                center: { lat: 40.7485722, lng: -74.0068633 },
-                zoom: 11,
+                center: { lat: 36.1521497, lng: -86.8201611 },
+                zoom: 12,
                 mapTypeId: 'roadmap',
             }
 
             this.map = new google.maps.Map(node, mapConfig);
+
+            // Add Markers to Map:
+            this.props.locations.forEach(location => {
+                const marker = new google.maps.Marker({
+                    position: location.location,
+                    map: this.map,
+                    title: location.name
+                });
+            })
 
         }
     }
