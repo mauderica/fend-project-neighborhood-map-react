15b4dbfa0a4de19ed682eba6404fe26af82a9ded
Mauderica <maude.chalin@gmail.com>
2018-04-26 14:44:52 -0400

    docs: add file & instructions to receive user's Foursquare client_id & client_secret

---
+++ my-neighborhood-map-app/src/credentials_example.js
@@ -0,0 +1,9 @@
+// Instructions:
+// 1. Replace the placeholder strings below with your client_id and client_secret
+// obtained from Foursquare Developers (https://developer.foursquare.com/docs/api/getting-started)
+// 2. Rename this file to credentials.js
+
+export default {
+    client_id: "YOUR_CLIENT_ID",
+    client_secret: "YOUR_CLIENT_SECRET",
+};
