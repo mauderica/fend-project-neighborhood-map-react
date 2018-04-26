ebad9d19eab1824783c0ef1cb6a2401439bfb614
Mauderica <maude.chalin@gmail.com>
2018-04-26 14:47:37 -0400

    docs(credentials_example.js): udpdate instructions

--- my-neighborhood-map-app/src/credentials_example.js
+++ my-neighborhood-map-app/src/credentials_example.js
@@ -1,7 +1,8 @@
 // Instructions:
-// 1. Replace the placeholder strings below with your client_id and client_secret
-// obtained from Foursquare Developers (https://developer.foursquare.com/docs/api/getting-started)
-// 2. Rename this file to credentials.js
+// 1. REPLACE the placeholder strings below with your client_id and client_secret to be
+// obtained from Foursquare Developers (https://developer.foursquare.com/docs/api/getting-started).
+// Refer to README.md "Instructions For Set-Up" for details.
+// 2. RENAME this file to "credentials.js"
 
 export default {
     client_id: "YOUR_CLIENT_ID",
